446d4afb14feaa202cda066d6483f37fb0bd4251
Antonio Malara <antonio.malara@booking.com>
2014-12-21 22:16:01 +0100

    changing to lists from objects

--- dumpers/pti.py
+++ dumpers/pti.py
@@ -26,127 +26,129 @@ def hexdump(data):
 def int_f(name, pos, v):
     return "%20s (%8x) ] %x (%d)" % (name, pos, v, v)
 
-class StructDesc(object):
-    def __init__(self):
-        self.structure = []
+########################
 
-    def read(f):
-        def wrap(self, name, *args, **kwargs):
-            self.structure.append((name, f, args, kwargs))
-        return wrap
+def evaluate(struct_desc, buf, pos=0, indent=0):
+    context = {}
+    return struct(None, struct_desc, buf, pos, indent, context)
 
-    def evaluate(self, buf, pos=0, indent=0):
-        output = []
-        all_size = 0
-        struct = {}
+def struct(name, struct_desc, buf, pos=0, indent=0, context={}):
+    output = []
+    all_size = 0
+    for field_desc in struct_desc:
+        type_func, name = field_desc[:2]
 
-        for name, f, args, kwargs in self.structure:
-            v, size, desc = f(self, name, buf, pos, *args, indent=indent, struct=struct, **kwargs)
+        try:    kwargs = field_desc[2]
+        except: kwargs = {}
 
-            struct[name] = v
+        v, size, desc = type_func(name, buf, pos, indent=indent, struct=context, **kwargs)
 
-            output.append(('    ' * indent) + desc)
-            pos += size
-            all_size += size
+        context[name] = v
 
-        return struct, all_size, '\n'.join(output)
+        output.append(('    ' * indent) + desc)
+        #print ('    ' * indent) + desc
+        pos += size
+        all_size += size
+
+    return context, all_size, '\n'.join(output)
 
 
-    @read
-    def padding(self, name, buf, pos, size, **kwargs):
-        return 0, size, ''
+def padding(name, buf, pos, size, **kwargs):
+    return 0, size, ''
 
-    @read
-    def uint8(self, name, buf, pos, **kwargs):
-        v = buf[pos]
-        return v, 1, int_f(name, pos, v)
+def uint8(name, buf, pos, **kwargs):
+    v = buf[pos]
+    return v, 1, int_f(name, pos, v)
 
-    @read
-    def uint16(self, name, buf, pos, **kwargs):
-        v  = buf[pos + 0]
-        v += buf[pos + 1] <<  8
-        return v, 2, int_f(name, pos, v)
+def uint16(name, buf, pos, **kwargs):
+    v  = buf[pos + 0]
+    v += buf[pos + 1] <<  8
+    return v, 2, int_f(name, pos, v)
 
-    @read
-    def uint32(self, name, buf, pos, **kwargs):
-        v  = buf[pos + 0]
-        v += buf[pos + 1] <<   8
-        v += buf[pos + 2] <<  16
-        v += buf[pos + 3] <<  24
-        return v, 4, int_f(name, pos, v)
+def uint32(name, buf, pos, **kwargs):
+    v  = buf[pos + 0]
+    v += buf[pos + 1] <<   8
+    v += buf[pos + 2] <<  16
+    v += buf[pos + 3] <<  24
+    return v, 4, int_f(name, pos, v)
 
-    @read
-    def array(self, name, buf, pos, items_struct, items=None, items_name=None, indent=0, struct={}):
+def array(name, buf, pos, items_struct={}, items=None, items_name=None, indent=0, struct={}):
 
-        if items:
-            out = "Fixed array of %d elements\n" % (items)
-        elif items_name:
-            
-            items = struct[items_name]
-            out ="variable array of %s (%d) elements\n" % (items_name, items)
-            
-        size = 0
-        result = []
-        out = [("%20s (%8x) ] %s" % (name, pos, out))]
-        for i in xrange(items):
-            out.append(('   ' * (indent + 1)) + 'Item %d:' % i)
-            v, s, o = items_struct.evaluate(buf, pos + size, indent + 1)
-            result.append(v)
-            out.append(o)
-            out.append('\n')
-            size += s
+    if items:
+        out = "Fixed array of %d elements\n" % (items)
+    elif items_name:
+        
+        items = struct[items_name]
+        out ="variable array of %s (%d) elements\n" % (items_name, items)
+        
+    size = 0
+    result = []
+    out = [("%20s (%8x) ] %s" % (name, pos, out))]
+    for i in xrange(items):
+        out.append(('   ' * (indent + 1)) + 'Item %d:' % i)
+        v, s, o = evaluate(items_struct, buf, pos + size, indent + 1)
+        result.append(v)
+        out.append(o)
+        out.append('\n')
+        size += s
 
-        return result, size, '\n'.join(out)
+    return result, size, '\n'.join(out)
 
-    @read
-    def string(self, name, buf, pos, length=None, length_name=None, xor=0, struct={}, **kwargs):
-        if not length:
-            length = struct[length_name]
-
-        v = ''.join(chr(i ^ xor) for i in buf[pos:pos + length])
-        desc = "%20s (%8x) ] %s" % (name, pos, v)
-        return v, length, desc
-
-    @read
-    def data(self, name, buf, pos, length_name=None, struct={}, **kwargs):
+def string(name, buf, pos, length=None, length_name=None, xor=0, struct={}, **kwargs):
+    if not length:
         length = struct[length_name]
-        v = ''.join(chr(i) for i in buf[pos:pos + length])
-        desc = "%20s (%8x) ] %s" % (name, pos, v)
-        return v, length, desc
+
+    v = ''.join(chr(i ^ xor) for i in buf[pos:pos + length])
+    desc = "%20s (%8x) ] %s" % (name, pos, v)
+    return v, length, desc
+
+def data(name, buf, pos, length_name=None, struct={}, **kwargs):
+    length = struct[length_name]
+    v = ''.join(chr(i) for i in buf[pos:pos + length])
+    desc = "%20s (%8x) ] %s" % (name, pos, v)
+    return v, length, desc
+
+###########
 
 ptifile = open(FILENAME, 'rb').read()
 ptifile = [ord(i) for i in ptifile]
 
 # hexdump(ptifile)
 
-infos = StructDesc()
-infos.uint16('first_line_in_this')
-infos.uint16('first_line_in_next')
-infos.uint16('nr_of_lines')
-infos.uint16('size_in_disk')
-infos.uint32('offset')
+pti_desc = (
+    (string,  'header_simul', {'length': 0x40}),
+    (array,   'infos', {
+        'items_struct': (
+            (uint16, 'first_line_in_this'),
+            (uint16, 'first_line_in_next'),
+            (uint16, 'nr_of_lines'),
+            (uint16, 'size_in_disk'),
+            (uint32, 'offset'),
+        ),
+        'items': 0x10,
+    }),
+    (uint16,  'header_unknown1'),
+    (uint8,   'header_xor_key'),
+    (uint8,   'header_unknown2'),
+    (padding, 'header_end_pad', {'size': 0x40}),
+)
 
-pti = StructDesc()
-pti.string('header_simul', 0x40)
-pti.array('infos', infos, items=0x10)
-pti.uint16('header_unknown1')
-pti.uint8('header_xor_key')
-pti.uint8('header_unknown2')
-pti.padding('header_end_pad', 0x40)
+obby, sizzi, outti = evaluate(pti_desc, ptifile) # evaluate the header
+print outti
 
+pti_block = (
+    (array, 'lines', {
+        'items_struct': (
+            (uint16, 'line_id'),
+            (uint8,  'length'),
+            (string, 'line', {'length_name': 'length', 'xor':obby['header_xor_key']}),
+            (uint8,  'unknown'),
+        ),
+        'items': obby['infos'][0]['nr_of_lines'],
+    }),
+)
 
-obby, sizzi, outti = pti.evaluate(ptifile) # evaluate the header
-
-line = StructDesc()
-line.uint16('line_id')
-line.uint8('length')
-line.string('line', length_name='length', xor=obby['header_xor_key'])
-line.uint8('padding')
-
-buff = StructDesc()
-buff.array('lines', line, items=obby['infos'][0]['nr_of_lines'])
-
-un, du, tr = buff.evaluate(ptifile, pos=obby['infos'][0]['offset'])
+un, du, tr = evaluate(pti_block, ptifile, pos=obby['infos'][0]['offset'])
 
 print tr
 
