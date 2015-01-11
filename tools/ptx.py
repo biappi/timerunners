0fb063aac8bb39d32ae9c6f5aab7006cb7f12402
Antonio Malara <antonio.malara@booking.com>
2015-01-11 18:15:29 +0100

    hints file reversed

---
+++ tools/ptx.py
@@ -0,0 +1,31 @@
+from dumper import *
+
+FILENAME = "../original/GAME_DIR/AR1/FIL/PTX"
+
+ptx_desc = (
+    (string, 'unknown', {'binary': fixed(True), 'length':fixed(0x10)}),
+        (uint16_be, 'count'),
+        (array,    'items', {
+            'items': relative('count'),
+            'items_struct': (
+                (uint16_be, 'room_nr'),
+                (uint16_be, 'pupo_min_x'),
+                (uint16_be, 'pupo_min_y'),
+                (uint16_be, 'pupo_max_x'),
+                (uint16_be, 'pupo_max_y'),
+                (uint16_be, 'nr_items'),
+                (array, 'ptis', {
+                    'items': fixed(5),
+                    'items_struct': (
+                        (uint16_be, 'look_in_swi_flag'),
+                        (uint16_be, 'pti1'),
+                        (uint16_be, 'pti2'),
+                    )
+                }),
+    )}),
+)
+
+if __name__ == '__main__':
+    dump_file(ptx_desc, FILENAME)
+
+
