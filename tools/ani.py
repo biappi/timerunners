c14a3cd12cbd144685249b6d8305f0fe3d99b92f
Antonio Malara <antonio.malara@booking.com>
2015-05-27 03:36:10 +0200

    some rendering of the anis

--- tools/ani.py
+++ tools/ani.py
@@ -1,6 +1,7 @@
 from dumper import *
 from ele import ele_desc, show_ele, draw_ele
 from pal import load_palette
+from imageutils import Image
 
 FILENAME = "../original/GAME_DIR/PLR/BNK/ANIX01.ANI"
 
@@ -42,13 +43,39 @@ ani_desc = (
     }),
 )
 
-if 0:
-    pal_file = "../original/GAME_DIR/AR1/STA/ARCADE.PAL"
-    palette = load_palette(pal_file)
-
+if 1:
     x = parse_file(ani_desc, FILENAME)
+
+    pal = ['#000000'] * 256
+
+    def color_dict_to_string(c):
+        return "#%02x%02x%02x" % (c['g'], c['b'], c['r'])
+
+    for i, color in  enumerate(x['palette'][0]['colors']):
+        print color
+        pal[i] = color_dict_to_string(color)
+
     ele1 = x['frames'][0]
-    draw_ele(ele1, 0)
+    width, height = ele1['width'] + 3, ele1['count']
+    img = Image(width, height)
+
+
+    ARCADE_PAL  = "../original/GAME_DIR/AR1/STA/ARCADE.PAL"
+    palette  = load_palette(ARCADE_PAL)
+
+    print 'w, h:', width, height
+
+    for y, l in enumerate(ele1['lines']):
+        line = l['line'][2:-1]
+
+        for x, c in enumerate(line):
+            try:
+                img.put(x, y, c)
+            except:
+                print "bad line", x, y, c
+
+    img.show(palette, 2)
+    
 
 else:
     dump_file(ani_desc, FILENAME)
